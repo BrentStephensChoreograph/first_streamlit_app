4dc2b23f4b1c7f6b102650d8b0e6d71bc9c0b4c6
BrentStephensChoreograph <101415070+BrentStephensChoreograph@users.noreply.github.com>
2023-02-15 12:06:08 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
-streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -13,7 +13,7 @@ my_cur = my_cnx.cursor()
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('",add_my_fruit, "')");
+my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
 
 
